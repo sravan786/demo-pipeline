a618c07de9fcbf1cd33bc1e91a6a74148c0901be
Sravan <sravan.lankoti@xyz.com>
2023-05-14 17:14:51 -0400

    pushing all the files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="census-income",
+      version="0.0.1",
+      author="sravan",
+      author_email="abc.xyz@gmail.com",
+      packages=find_packages(),
+      install_reruires=[]
+)
